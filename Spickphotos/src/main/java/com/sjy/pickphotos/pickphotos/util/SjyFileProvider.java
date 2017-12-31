76874ca3013b03594e0a0d490e4cc4c15db1ee20
shajinyang <877672787@qq.com>
2017-12-31 14:28:54 +0800

    fileprovider  优化

---
+++ Spickphotos/src/main/java/com/sjy/pickphotos/pickphotos/util/SjyFileProvider.java
@@ -0,0 +1,10 @@
+package com.sjy.pickphotos.pickphotos.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by sjy on 2017/12/31.
+ */
+
+public class SjyFileProvider extends FileProvider {
+}
